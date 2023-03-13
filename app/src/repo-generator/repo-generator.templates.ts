b1467d692369a33286b4e20eb69613b362b73756
David Hardy <david.hardy@onn.software>
2023-03-13 11:54:56 +0100

    Mappers for input fields

--- app/src/repo-generator/repo-generator.templates.ts
+++ app/src/repo-generator/repo-generator.templates.ts
@@ -13,12 +13,14 @@ export abstract class OnnBaseRepo<SQL_TYPE extends {}> {
   paginate = async (
     qb: model.QueryBuilder<SQL_TYPE>,
     fields: string | string[] = '*',
-    paginate: model.Paginate
+    paginate: model.Paginate,
+    orderBy?: { field: string, direction: 'asc' | 'desc' },
   ): Promise<model.Paginated<SQL_TYPE>> => {
     const totalEntries = await qb.executeCount();
     
+    const _qb = qb.select(fields).orderBy(orderBy);
     const noLimit = paginate.pageSize <= 0;
-    const query = noLimit ? qb.select(fields) : qb.select(fields).offset(paginate.pageIndex * paginate.pageSize).limit(paginate.pageSize);
+    const query = noLimit ? _qb : _qb.offset(paginate.pageIndex * paginate.pageSize).limit(paginate.pageSize);
     const data = await query.execute();
 
     return {
@@ -32,7 +34,9 @@ export abstract class OnnBaseRepo<SQL_TYPE extends {}> {
 
 `;
 
-export const repoTemplate = `export class __SQL_TYPE___Repo extends OnnBaseRepo<model.__SQL_TYPE__> {
+export const repoTemplate = `
+__UNSAFE_LOOKUP__
+export class __SQL_TYPE___Repo extends OnnBaseRepo<model.__SQL_TYPE__> {
   constructor() {
     super('__SQL_TABLE__');
   }
@@ -43,8 +47,9 @@ export const repoTemplate = `export class __SQL_TYPE___Repo extends OnnBaseRepo<
     orderBy?: { field: string, direction: 'asc' | 'desc' },
     fields: string | string[] = '*',
   ): Promise<model.__SQL_TYPE__> {
+__UNSAFE_MAPPERS__
     const [res] = await this.builder().select(fields).where(key, value).orderBy(orderBy).execute() as any;
-__MAPPERS__
+__SAFE_MAPPERS__
     return res;
   }
 
@@ -55,9 +60,11 @@ __MAPPERS__
     fields: string | string[] = '*',
     builder: (qb: model.QueryBuilder<model.__SQL_TYPE__>) => model.QueryBuilder<model.__SQL_TYPE__> = qb => qb
   ): Promise<model.Paginated<model.__SQL_TYPE__>> {
-    const queryBuilder = clauses.reduce((qb, c) => qb.whereIn(c.key, c.values), this.builder()).orderBy(orderBy)
-    const paginated = await this.paginate(builder(queryBuilder), fields, paginate ?? { pageIndex: -1, pageSize: -1 });
-__PAGINATED_MAPPERS__
+__UNSAFE_MAPPERS__
+__UNSAFE_PAGINATED_MAPPERS__
+    const queryBuilder = clauses.reduce((qb, c) => qb.whereIn(c.key, c.values), this.builder())
+    const paginated = await this.paginate(builder(queryBuilder), fields, paginate ?? { pageIndex: -1, pageSize: -1 }, orderBy);
+__SAFE_PAGINATED_MAPPERS__
     return paginated;
   }
 }
--- app/src/repo-generator/repo-generator.ts
+++ app/src/repo-generator/repo-generator.ts
@@ -1,5 +1,5 @@
 import { baseRepo, repoTemplate } from './repo-generator.templates';
-import { TableDef } from '../model';
+import { TableColDef, TableDef } from '../model';
 import { Globals } from '../globals';
 
 export class RepoGenerator {
@@ -12,16 +12,19 @@ export class RepoGenerator {
     const interfaceName = Globals.getTypescriptName(tableDef.tableName);
 
     const remapKeys = tableDef.columns.filter((c) => !!c.sqlKey);
-    const mappers = remapKeys
+
+    const { lookupTable, unSafeMappers, unSafePaginatedMappers } = this.buildUnsafeMappers(interfaceName, remapKeys);
+
+    const safeMappers = remapKeys
       .map((c) => {
         const safeSqlKey = c.sqlKey?.startsWith('`') ? c.sqlKey : `\`${c.sqlKey}\``;
         return `    res.${c.key} = res[${safeSqlKey}];\n    delete res[${safeSqlKey}]`;
       })
       .join('\n');
-    const paginatedMappers =
+    const safePaginatedMappers =
       remapKeys.length === 0
         ? ''
-        : `    paginated.data = paginated.data.map((res: any) => {\n${mappers.replaceAll(
+        : `    paginated.data = paginated.data.map((res: any) => {\n${safeMappers.replaceAll(
             '    ',
             '      '
           )}
@@ -39,9 +42,33 @@ export class RepoGenerator {
       .replaceAll('__SQL_TABLE__', tableDef.tableName)
       .replaceAll('__UNIQUE_FIELDS__', unqiueKeys)
       .replaceAll('__NON_UNIQUE_FIELDS__', allKeys)
-      .replaceAll('__MAPPERS__', mappers)
-      .replaceAll('__PAGINATED_MAPPERS__', paginatedMappers)
+      .replaceAll('__UNSAFE_LOOKUP__', lookupTable)
+      .replaceAll('__UNSAFE_MAPPERS__', unSafeMappers)
+      .replaceAll('__UNSAFE_PAGINATED_MAPPERS__', unSafePaginatedMappers)
+      .replaceAll('__SAFE_MAPPERS__', safeMappers)
+      .replaceAll('__SAFE_PAGINATED_MAPPERS__', safePaginatedMappers)
       .replaceAll('__FIELD_GETTER_BLOCK__', '') // fieldKeyBlocks.join('\n'))
       .replaceAll('__PAGINATED_FIELD_GETTER_BLOCK__', ''); // paginatedFieldKeyBlocks.join('\n'));
+  }
+
+  private buildUnsafeMappers(
+    interfaceName: string,
+    remapKeys: TableColDef[]
+  ): { lookupTable: string; unSafeMappers: string; unSafePaginatedMappers: string } {
+    if (remapKeys.length < 0) {
+      return { lookupTable: '', unSafeMappers: '', unSafePaginatedMappers: '' };
+    }
+
+    const lookupTable = `
+    const ${interfaceName}FieldLookUp: Record<string, string> = {
+${remapKeys.map((c) => `      ${c.key}: ${c.sqlKey}`).join(',\n')}
+    };
+    
+    `;
+
+    const unSafeMappers = `    if(orderBy?.field) orderBy.field = ${interfaceName}FieldLookUp[orderBy.field] ?? orderBy.field;`;
+    const unSafePaginatedMappers = `    clauses = clauses.map(clause => ({...clause, key: ${interfaceName}FieldLookUp[clause.key] ?? clause.key}));`;
+
+    return { lookupTable, unSafeMappers, unSafePaginatedMappers };
   }
 }
